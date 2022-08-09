310c5c98c18fdad1ea099c4afdbf2b004f57ef07
ltc01 <54469264+ltc01@users.noreply.github.com>
2022-08-09 19:23:59 +0530

    Create shipping.py

---
+++ shipping.py
@@ -0,0 +1,30 @@
+#sal's shipping
+
+weight = 41.5
+
+#Ground shipping
+if weight <= 2:
+  cost =weight*1.50+20
+elif weight<=6:
+  cost=weight*3.00+20
+elif weight<=10:
+  cost=weight*4.00+20
+else:
+  cost=weight*4.75+20
+print("Ground shipping: $" +str(cost))
+
+#Premium ground shipping
+cost_ground_shipping=125.00
+
+print("Ground Shipping Premium: $"+str(cost_ground_shipping))
+
+#drone shipping
+if weight <= 2:
+  cost_droneShipping =weight*4.50
+elif weight<=6:
+  cost_droneShipping =weight*9.00
+elif weight<=10:
+  cost_droneShipping =weight*12.00
+else:
+  cost_droneShipping =weight*14.25
+print("Drone shipping cost: $" +str(cost_droneShipping ))
